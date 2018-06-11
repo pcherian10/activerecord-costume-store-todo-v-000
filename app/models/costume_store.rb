8003ce67cf0cbbc82929e64da67e71ac601a4015
Paul <pcherian10@gmail.com>
2018-06-11 22:17:02 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,4 +1,13 @@
 # Create your CostumeStore class here
-class CostumeStore
+class CostumeStore < ActiveRecord::Base
+    def change
+      create_table (:costume) do |t|
+        t.string :name
+        t.float :price
+        t.string :size
+        t.string :image_url
+      end
+     end
+  end
 
 end
