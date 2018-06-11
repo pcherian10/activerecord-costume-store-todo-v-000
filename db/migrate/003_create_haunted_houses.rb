8404b6e82303d0b78f1dd21284074fdccce1dbf0
Paul <pcherian10@gmail.com>
2018-06-11 22:42:56 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -13,6 +13,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.string :long_description
+      t.timestamps
   end
 
  end
